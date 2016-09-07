c922dcc87fb98aea49587dcf5d11bed86b122520
Karina Gonzalez <karinamzalez@gmail.com>
2016-09-07 13:37:14 -0600

    add transactions index route

--- config/routes.rb
+++ config/routes.rb
@@ -16,9 +16,10 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1, defaults: {format: :json} do
-      resources :invoices,  only: [:index, :show]
-      resources :merchants, only: [:index, :show]
-      resources :items,     only: [:index, :show]
+      resources :invoices,     only: [:index, :show]
+      resources :merchants,    only: [:index, :show]
+      resources :transactions, only: [:index, :show]
+      resources :items,        only: [:index]
     end
   end
 end
