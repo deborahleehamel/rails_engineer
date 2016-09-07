d5036a24580e8a79cf8bc9a177e2847e4ccd2312
Karina Gonzalez <karinamzalez@gmail.com>
2016-09-07 10:44:45 -0600

    fixed depracation warning in merchant/find_controller_spec passed in params: --- when specifying specific params

--- spec/controllers/api/v1/merchants/find_controller_spec.rb
+++ spec/controllers/api/v1/merchants/find_controller_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Api::V1::Merchants::FindController do
   describe "GET index" do
     it "can get all instances of merchant by ID" do
       merchant =  merchants(:two)
-      get :index, id: merchant.id
+      get :index, params: { id: merchant.id }
 
       parsed_merchants = JSON.parse(response.body)
 
@@ -15,12 +15,23 @@ RSpec.describe Api::V1::Merchants::FindController do
 
     it "can get all instances of merchant by name" do
       merchant = merchants(:three)
-      get :index, name: merchant.name
+      get :index, params: { name: merchant.name }
 
       parsed_merchants = JSON.parse(response.body)
 
       expect(parsed_merchants.count).to eq 2
       expect(parsed_merchants.first["name"]).to eq "Matel"
+    end
+
+    it "can get all instances of merchant by created_at" do
+      merchant = merchants(:two)
+      get :index, params: { created_at: merchant.created_at }
+
+      parsed_merchants = JSON.parse(response.body)
+
+      expect(parsed_merchants.count).to eq 2
+      expect(parsed_merchants.first["name"]).to eq "Programmers R Us"
+      expect(parsed_merchants.second["name"]).to eq "Programmers R Us 2"
     end
   end
 end
