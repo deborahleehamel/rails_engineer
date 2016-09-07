c783acb495ce8bed3ecfe87db4b50cc91952c77f
Karina Gonzalez <karinamzalez@gmail.com>
2016-09-07 13:30:04 -0600

    create transactions.yml

--- spec/controllers/api/v1/transactions_controller_spec.rb
+++ spec/controllers/api/v1/transactions_controller_spec.rb
@@ -5,12 +5,12 @@ RSpec.describe Api::V1::TransactionsController do
   describe "GET index" do
     it "can get all instances of transaction" do
       get :index
-      
+
       assert_response :success
 
-      parsed_merchants = JSON.parse(response.body)
+      parsed_transactions = JSON.parse(response.body)
 
-      expect(parsed_merchants.count).to eq 5
+      expect(parsed_transactions.count).to eq 5
     end
   end
 end
