30d70683dc5bae7539ee8dc383d5d4c79fbb01b0
Deborah Hamel <deborahleehamel@gmail.com>
2016-09-07 00:23:07 -0600

    add find_controller to api/v1/items add nested route for items to /find add show method and params to items_controller

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,14 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1, defaults: {format: :json} do
+      namespace :items do
+        get "/find",     to: "find#show"
+      end
+    end
+  end
+
+  namespace :api do
+    namespace :v1, defaults: {format: :json} do
       resources :invoices,  only: [:index, :show]
       resources :merchants, only: [:index, :show]
       resources :items,     only: [:index, :show]
