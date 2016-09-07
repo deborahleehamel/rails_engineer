14d5a5aca0c69c3d402ef30f5d5fd25afd30b042
Karina Gonzalez <karinamzalez@gmail.com>
2016-09-07 14:53:20 -0600

    customer single and multi finders

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,10 @@ Rails.application.routes.draw do
         get "/find",      to: "find#show"
         get "/find_all",  to: "find#index"
       end
+      namespace :customers do
+        get "/find",      to: "find#show"
+        get "/find_all",  to: "find#index"
+      end
     end
   end
 
