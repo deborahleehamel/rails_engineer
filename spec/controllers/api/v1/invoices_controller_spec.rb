f620501acff7fa77b2746166ec2a7c2582001304
Deborah Hamel <deborahleehamel@gmail.com>
2016-09-06 19:32:56 -0600

    add invoices show method add test to get a single invoice add route to show action

--- spec/controllers/api/v1/invoices_controller_spec.rb
+++ spec/controllers/api/v1/invoices_controller_spec.rb
@@ -7,9 +7,25 @@ RSpec.describe Api::V1::InvoicesController do
       get :index
 
       assert_response :success
+
       parsed_invoices = JSON.parse(response.body)
 
       expect(parsed_invoices.count).to eq 6
+    end
+  end
+
+  describe "GET show" do
+    it "can get a single invoice" do
+      invoice = invoices(:one)
+      get :show, id: invoice.id
+
+      assert_response :success
+
+      parsed_invoice = JSON.parse(response.body)
+
+      expect(parsed_invoice["status"]).to      eq invoice.status
+      expect(parsed_invoice["customer_id"]).to eq invoice.customer_id
+      expect(parsed_invoice["merchant_id"]).to eq invoice.merchant_id
     end
   end
 end
