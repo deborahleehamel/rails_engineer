29ae61916ca86146fd64df642a8f60a2374d01f0
Deborah Hamel <deborahleehamel@gmail.com>
2016-09-06 20:54:09 -0600

    add invoices random_controller add new route to api/v1/invoices namespace for /random add new method for random_id in invoice model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,11 +2,15 @@ class Invoice < ApplicationRecord
   validates :customer_id, presence: true
   validates :merchant_id, presence: true
   validates :status, presence: true
-  
+
   belongs_to :merchant
   belongs_to :customer
 
   has_many   :transactions
   has_many   :invoice_items
   has_many   :items, through: :invoice_items
+
+  def self.random_id
+    pluck(:id).shuffle.pop
+  end
 end
