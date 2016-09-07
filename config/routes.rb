2645c2da7a2b46dfec27ca710e8018de78194454
Karina Gonzalez <karinamzalez@gmail.com>
2016-09-07 14:48:27 -0600

    transaction multi-finders

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
       end
       namespace :transactions do
         get "/find",      to: "find#show"
+        get "/find_all",  to: "find#index"
       end
     end
   end
