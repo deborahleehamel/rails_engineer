a6ef7ce93d754154f69f094aab95f80522954219
Deborah Hamel <deborahleehamel@gmail.com>
2016-09-08 15:59:34 -0600

    edit indentation

--- app/controllers/api/v1/items/best_day_controller.rb
+++ app/controllers/api/v1/items/best_day_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::Items::BestDayController < ApplicationController
 
   private
 
-  def item
-    Item.find(params[:id])
-  end
+    def item
+      Item.find(params[:id])
+    end
 end
