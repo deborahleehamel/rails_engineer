a90d44947e7af66a736991ef5119145d6079616a
Karina Gonzalez <karinamzalez@gmail.com>
2016-09-08 23:10:52 -0600

    most items sold

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
         get "/find",              to: "find#show"
         get "/find_all",          to: "find#index"
         get "/most_items",        to: "most_items#index"
+        get "/most_revenue",      to: "most_revenues#index"
         get "/:id/invoice_items", to: "invoice_items#index"
         get "/:id/merchant",      to: "merchants#show"
         get "/:id/best_day",      to: "best_day#show"
