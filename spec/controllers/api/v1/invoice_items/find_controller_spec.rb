befc53a419c863bf34cd0d778c20bad887fe77d6
Deborah Hamel <deborahleehamel@gmail.com>
2016-09-08 23:00:31 -0600

    add invoice_items single finders and multifinders add find controller to invoice_items add tests for index and show add nested routes for /find and /find_all in invoice_items namespace

--- spec/controllers/api/v1/invoice_items/find_controller_spec.rb
+++ spec/controllers/api/v1/invoice_items/find_controller_spec.rb
@@ -15,51 +15,50 @@ RSpec.describe Api::V1::InvoiceItems::FindController do
 
     end
 
-    it "can get all invoices by ITEM_ID" do
+    it "can get all invoice_items by ITEM_ID" do
       invoice_item = invoice_items(:one)
       get :index, params: { item_id: invoice_item.item_id }
 
       parsed_invoice_items = JSON.parse(response.body)
 
-      expect(parsed_invoice_items.count).to                eq 1
+      expect(parsed_invoice_items.count).to            eq 1
       expect(parsed_invoice_items.first["item_id"]).to eq 298486374
       expect(parsed_invoice_items.last["item_id"]).to  eq 298486374
     end
 
-    it "can get all invoices by INVOICE_ID" do
+    it "can get all invoice_items by INVOICE_ID" do
       invoice_item = invoice_items(:one)
       get :index, params: { invoice_id: invoice_item.invoice_id }
 
       parsed_invoice_items = JSON.parse(response.body)
 
-      expect(parsed_invoice_items.count).to                eq 2
+      expect(parsed_invoice_items.count).to               eq 2
       expect(parsed_invoice_items.first["invoice_id"]).to eq 980190962
       expect(parsed_invoice_items.last["invoice_id"]).to  eq 980190962
     end
 
-    it "can get all invoices by QUANTITY" do
-      invoice = invoices(:one)
+    it "can get all invoice_items by QUANTITY" do
+      invoice_item = invoice_items(:one)
       get :index, params: { quantity: invoice_item.quantity }
 
       parsed_invoice_items = JSON.parse(response.body)
 
-      expect(parsed_invoice_items.count).to eq 4
-      expect(parsed_invoice_items.first["quantity"]).to eq 980190962
-      expect(parsed_invoice_items.last["quantity"]).to  eq 980190962
+      expect(parsed_invoice_items.count).to eq 2
+      expect(parsed_invoice_items.first["quantity"]).to eq 3
+      expect(parsed_invoice_items.last["quantity"]).to  eq 3
     end
 
-    it "can get all invoices by UNIT_PRICE" do
+    it "can get all invoice_items by UNIT_PRICE" do
       invoice_item = invoice_items(:one)
-      get :index, params: { unit_price: invoice.unit_price }
+      get :index, params: { unit_price: invoice_item.unit_price }
 
       parsed_invoice_items = JSON.parse(response.body)
 
-      expect(parsed_invoice_items.count).to eq 2
-      expect(parsed_invoice_items.first["quantity"]).to eq "shipped"
-      expect(parsed_invoice_items.last["quantity"]).to  eq "shipped"
+      expect(parsed_invoice_items.count).to               eq 1
+      expect(parsed_invoice_items.first["unit_price"]).to eq 9
     end
 
-    it "can get all invoices by CREATED_AT" do
+    it "can get all invoice_items by CREATED_AT" do
       invoice_item = invoice_items(:one)
       get :index, params: { created_at: invoice_item.created_at }
 
@@ -67,18 +66,18 @@ RSpec.describe Api::V1::InvoiceItems::FindController do
 
       expect(parsed_invoice_items.count).to eq 2
       expect(parsed_invoice_items.first["quantity"]).to eq 3
-      expect(parsed_invoice_items.last["item_id"]).to  eq "one"
+      expect(parsed_invoice_items.last["item_id"]).to   eq 980190962
     end
 
-    it "can get all invoices by UPDATED_AT" do
+    it "can get all invoice_items by UPDATED_AT" do
       invoice_item = invoice_items(:one)
       get :index, params: { updated_at: invoice_item.updated_at }
 
-      parsed_invoices = JSON.parse(response.body)
+      parsed_invoice_items = JSON.parse(response.body)
 
       expect(parsed_invoice_items.count).to             eq 2
       expect(parsed_invoice_items.first["quantity"]).to eq 3
-      expect(parsed_invoice_items.last["item_id"]).to      eq "one"
+      expect(parsed_invoice_items.last["item_id"]).to   eq 980190962
     end
   end
 
@@ -89,47 +88,59 @@ RSpec.describe Api::V1::InvoiceItems::FindController do
 
       assert_response :success
 
-      parsed_invoice = JSON.parse(response.body)
+      parsed_invoice_item = JSON.parse(response.body)
 
-      expect(parsed_invoice_item["id"]).to      eq 298486374
-      expect(parsed_invoice_item["id"]).to      eq invoice_item.id
-      expect(parsed_invoice_item["status"]).to  eq invoice_item.status
-    end
-
-    it "can get a single instance of invoice by CUSTOMER ID" do
-      invoice = invoices(:two)
-      get :show, params: { customer_id: invoice.customer_id }
-
-      assert_response :success
-
-      parsed_invoice = JSON.parse(response.body)
-
-      expect(parsed_invoice["customer_id"]).to eq 298486374
-    end
-
-    it "can get a single instance of invoice by MERCHANT ID" do
-      invoice = invoices(:two)
-      get :show, params: { merchant_id: invoice.merchant_id }
-
-      assert_response :success
-
-      parsed_invoice = JSON.parse(response.body)
-
-      expect(parsed_invoice["merchant_id"]).to eq 980190962
-    end
-
-    it "can get a single instance of invoice by STATUS" do
-      invoice = invoices(:two)
-      get :show, params: { status: invoice.status }
-
-      assert_response :success
-
-      parsed_invoice = JSON.parse(response.body)
-
+      expect(parsed_invoice_item["id"]).to          eq 298486374
       expect(parsed_invoice_item["id"]).to          eq invoice_item.id
-      expect(parsed_invoice_item["status"]).to      eq invoice_item.status
-      expect(parsed_invoice_item["customer_id"]).to eq invoice_item.customer_id
-      expect(parsed_invoice_item["merchant_id"]).to eq invoice_item.merchant_id
+      expect(parsed_invoice_item["quantity"]).to    eq 7
+      expect(parsed_invoice_item["unit_price"]).to  eq 6
+
+    end
+
+    it "can get a single instance of invoice by ITEM_ID" do
+      invoice_item = invoice_items(:two)
+      get :show, params: { item_id: invoice_item.item_id }
+
+      assert_response :success
+
+      parsed_invoice_item = JSON.parse(response.body)
+
+      expect(parsed_invoice_item["item_id"]).to eq 980190962
+    end
+
+    it "can get a single instance of invoice by INVOICE_ID" do
+      invoice_item = invoice_items(:two)
+      get :show, params: { invoice_id: invoice_item.invoice_id }
+
+      assert_response :success
+
+      parsed_invoice_item = JSON.parse(response.body)
+
+      expect(parsed_invoice_item["invoice_id"]).to eq 298486374
+    end
+
+    it "can get a single instance of invoice by QUANTITY" do
+      invoice_item = invoice_items(:two)
+      get :show, params: { quuantity: invoice_item.quantity }
+
+      assert_response :success
+
+      parsed_invoice_item = JSON.parse(response.body)
+
+      expect(parsed_invoice_item["quantity"]).to eq 3
+    end
+
+    it "can get a single instance of invoice by UNIT_PRICE" do
+      invoice_item = invoice_items(:two)
+      get :show, params: { unit_price: invoice_item.unit_price }
+
+      assert_response :success
+
+      parsed_invoice_item = JSON.parse(response.body)
+
+      expect(parsed_invoice_item["id"]).to         eq invoice_item.id
+      expect(parsed_invoice_item["unit_price"]).to eq 6
+      expect(parsed_invoice_item["quantity"]).to   eq 7
     end
 
     it "can get a single instance of invoice by CREATED_AT" do
@@ -138,11 +149,10 @@ RSpec.describe Api::V1::InvoiceItems::FindController do
 
       assert_response :success
 
-      parsed_invoice = JSON.parse(response.body)
+      parsed_invoice_item = JSON.parse(response.body)
 
-      expect(parsed_invoice_items.count).to             eq 2
-      expect(parsed_invoice_items.first["quantity"]).to eq 3
-      expect(parsed_invoice_item["id"]).to              eq invoice_item.id
+      expect(parsed_invoice_item["quantity"]).to eq 7
+      expect(parsed_invoice_item["id"]).to       eq invoice_item.id
     end
 
     it "can get a single instance of invoice by UPDATED_AT" do
@@ -151,11 +161,10 @@ RSpec.describe Api::V1::InvoiceItems::FindController do
 
       assert_response :success
 
-      parsed_invoice = JSON.parse(response.body)
+      parsed_invoice_item = JSON.parse(response.body)
 
-      expect(parsed_invoice_items.count).to             eq 2
-      expect(parsed_invoice_items.first["quantity"]).to eq 3
-      expect(parsed_invoice_item["id"]).to              eq invoice_item.id
+      expect(parsed_invoice_item["quantity"]).to eq 7
+      expect(parsed_invoice_item["id"]).to       eq invoice_item.id
     end
   end
 end
